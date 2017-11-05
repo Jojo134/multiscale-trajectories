88852a86725b874fc2ec2cc65370b04977041c03
Joachim Spalink <J.spalink@gmx.de>
2017-11-05 16:04:25 +0100

    change class to not interfere with others, show axis on bar chart

--- src/app/shared/barchart/barchart.component.ts
+++ src/app/shared/barchart/barchart.component.ts
@@ -11,7 +11,7 @@ export class BarChartComponent implements OnInit, OnChanges {
   @ViewChild('chart') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
 
-  private margin: any = { top: 2, bottom: 2, left: 2, right: 2 };
+  private margin: any = { top: 2, bottom: 2, left: 20, right: 2 };
   private chart: any;
   private width: number;
   private height: number;
@@ -40,7 +40,7 @@ export class BarChartComponent implements OnInit, OnChanges {
     const element = this.chartContainer.nativeElement;
     // this.width = element.offsetWidth - this.margin.left - this.margin.right;
     // this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
-    this.width = 500;
+    this.width = 500 - this.margin.left - this.margin.right;
     this.height = 50;
 
     const svg = d3.select(element).append('svg')
@@ -61,6 +61,15 @@ export class BarChartComponent implements OnInit, OnChanges {
 
     // bar colors
     this.colors = d3.scaleLinear().domain([0, this.data.length]).range(<any[]>['red', 'blue']);
+    // x & y axis
+    //this.xAxis = svg.append('g')
+    //.attr('class', 'axis axis-x')
+    //.attr('transform', `translate(${this.margin.left}, ${this.margin.top + this.height})`);
+    // .call(d3.axisBottom(this.xScale));
+    this.yAxis = svg.append('g')
+      .attr('class', 'axis axis-y')
+      .attr('transform', `translate(${this.width}, ${this.margin.top})`)
+      .call(d3.axisRight(this.yScale));
 
   }
 
@@ -69,6 +78,10 @@ export class BarChartComponent implements OnInit, OnChanges {
     this.xScale.domain(this.data.map(d => d[0]));
     this.yScale.domain([0, d3.max(this.data, d => d[1])]);
     this.colors.domain([0, this.data.length]);
+    //this.xAxis.transition();
+    // .call(d3.axisBottom(this.xScale));
+    this.yAxis.transition()
+      .call(d3.axisRight(this.yScale));
 
     const update = this.chart.selectAll('.bar')
       .data(this.data);
