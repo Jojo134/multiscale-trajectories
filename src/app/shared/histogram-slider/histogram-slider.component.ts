7c795cf2da0e95ff9257e9091fbf7756044f5652
Joachim Spalink <J.spalink@gmx.de>
2017-11-07 17:16:23 +0100

    changing max value to correct and removing not needed data

--- src/app/shared/histogram-slider/histogram-slider.component.ts
+++ src/app/shared/histogram-slider/histogram-slider.component.ts
@@ -13,9 +13,6 @@ export class HistogramSliderComponent implements OnInit {
   chartData;
   someRange = [0, 0];
   constructor() {
-    const data = [['Index 0', 0.1], ['Index 1', 0.2], ['Index 2', 0.3], ['Index 3', 0.4],
-    ['Index 4', 0.5], ['Index 5', 0.6], ['Index 6', 0.7], ['Index 7', 0.8], ['Index 8', 0.9], ['Index 9', 1]];
-    this.chartData = data;
   }
   ngOnInit() {
   }
