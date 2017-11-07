ae5018a5624abb1e9bf1f7b8767ab2bd529802fe
Joachim Spalink <J.spalink@gmx.de>
2017-11-07 16:02:13 +0100

    cluster visible

--- src/app/cluster/cluster.component.ts
+++ src/app/cluster/cluster.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgProgress } from 'ngx-progressbar';
 
-import { SelectionService, DataService, MultiMatch, calcdiag } from '../shared';
+import { SelectionService, DataService, MultiMatch, calcdiag, stringToColor } from '../shared';
 import { QTree, Trajectory, TrajectoryViewType } from '../data-structures';
 import * as ml from 'machine_learning';
 
@@ -11,7 +11,14 @@ import * as ml from 'machine_learning';
   styleUrls: ['./cluster.component.css']
 })
 export class ClusterComponent implements OnInit {
+  visibleData: Array<TrajectoryViewType> = [];
   someRange = 3;
+  cluster_list;
+  clusteredData = [];
+  idList;
+  clustered_stimuli = [];
+  k = 2;
+  epochs = 100;
   private chartData: Array<any>;
   nrLines: number;
   minQuadsize = 20;
@@ -21,9 +28,6 @@ export class ClusterComponent implements OnInit {
   viewAsQuadtree: boolean;
   selected_participants = Array<{ name: string, id: number }>();
   selected_stimuli = Array<{ name: string, id: number }>();
-  resolutionName = '/assets/resolution.txt';
-  filenameall = 'assets/all_fixation_data_cleaned_up.csv';
-  filename = 'assets/small_fix_data_cleaned.csv';
   filteredFixData: Array<TrajectoryViewType> = [];
   resolutions: Array<{ city: string, height: number, width: number }> = [];
   qTree: QTree;
@@ -40,19 +44,15 @@ export class ClusterComponent implements OnInit {
     this.participants = this.dataService.getParticioants();
     this.stimuli = this.dataService.getStimuli();
     this.filteredFixData = this.dataService.filterData({ asQuad: this.viewAsQuadtree, level: this.someRange });
+    this.visibleData = this.filteredFixData;
+  }
 
-  }
-  generateData() {
-    this.chartData = [];
-    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
-      this.chartData.push([`Index ${i}`, Math.floor(Math.random() * 100)]);
-    }
-  }
   filterChangeParticipant(selected: any[]) {
     console.log(selected);
     this.selected_participants = selected;
     this.selectionService.setSelectedParticipants(selected);
     this.filteredFixData = this.dataService.filterData({ asQuad: this.viewAsQuadtree, level: this.someRange });
+    this.visibleData = this.filteredFixData;
   }
 
   filterChangeStimuli(selected: any[]) {
@@ -60,45 +60,80 @@ export class ClusterComponent implements OnInit {
     this.selected_stimuli = selected;
     this.selectionService.setSelectedStimuli(selected);
     this.filteredFixData = this.dataService.filterData({ asQuad: this.viewAsQuadtree, level: this.someRange });
+    this.visibleData = this.filteredFixData;
   }
+
+  filterChangeCluster(selected: any[]) {
+    console.log(selected);
+    this.clustered_stimuli = [];
+    this.visibleData = [];
+    selected.forEach(e => {
+      this.clustered_stimuli.push(...this.clusteredData.filter(c => e.name === c.cluster));
+      this.visibleData.push(...this.clusteredData.filter(c => e.name === c.cluster));
+    });
+  }
+
+  filterChangeClusterStimuli(selected: any[]) {
+    console.log(selected);
+    const checkdata = selected.map(s => s.data);
+    this.visibleData = [];
+    this.visibleData = selected.map(d => ({ stimulus: d.data.s, participant: d.data.p, color: d.color, points: d.points }));
+  }
+
   cluster() {
     this.simScores = this.computeSimScores();
     console.log('sim scores done');
     const data = [];
+    this.idList = [];
     this.ngProgress.start();
     for (let i = 0; i < this.filteredFixData.length; i++) {
       for (let k = i + 1; k < this.filteredFixData.length; k++) {
         data.push(Object.values(this.simScores[i][k].scores));
+        this.idList.push(this.simScores[i][k].p1 +
+          ' ' + this.simScores[i][k].s1 + ' ' + this.simScores[i][k].p2 + ' ' + this.simScores[i][k].s2);
       }
       this.ngProgress.set(i / this.filteredFixData.length);
     }
     this.ngProgress.done();
     console.log('data transform done');
-    console.log(data);
+
     this.ngProgress.start();
     const result = ml.kmeans.cluster({
       data: data,
-      // k: data.length > 20 ? 20 : data.length,
-      k: 2,
-      epochs: 100,
+      k: this.k,
+      epochs: this.epochs,
       distance: { type: 'euclidean' }
     });
     this.ngProgress.done();
-    // arrfjson {header:{ relation: , attributes:[{},{}],data:[{},{}]}}}
-    // list participant, stimuli as nominal
     console.log('clusters : ', result.clusters);
     console.log('means : ', result.means);
     result.clusters.forEach(element => {
-      element.forEach(e => console.log(this.findCorrespondingPair(e)));
+      element.forEach(e => console.log(element, this.idList[e]));
     });
-    // console.log(simScores);
-    // console.log(JSON.stringify(simScores));
+
+    let count = 0;
+
+    this.cluster_list = result.clusters.map((v, i) => ({ id: i, name: 'cluster ' + i, values: v }));
+
+
+    this.clusteredData = [];
+    this.cluster_list.forEach(e => e.values.forEach(v => {
+      const splitted = this.idList[v].split(' ');
+      const names = [{ p: splitted[0], s: splitted[1] }, { p: splitted[2], s: splitted[3] }];
+      this.clusteredData.push(...names.map(n => ({
+        id: count++,
+        name: n.p + ' ' + n.s,
+        data: n,
+        cluster: e.name,
+        color: stringToColor.next(e.name),
+        points: this.filteredFixData.find(d => d.participant === n.p && d.stimulus === n.s).points
+      })
+      ));
+    }));
+    console.log(this.clusteredData);
+    this.visibleData = this.clusteredData.map(d => ({ stimulus: d.data.s, participant: d.data.p, color: d.color, points: d.points }));
   }
-  findCorrespondingPair(clusterIndex: number) {
-    const row = clusterIndex % this.simScores.length;
-    const col = clusterIndex / this.simScores[0].length;
-    return this.simScores[row][col + row];
-  }
+
   computeSimScores() {
     const simScores = [];
     this.ngProgress.start();
