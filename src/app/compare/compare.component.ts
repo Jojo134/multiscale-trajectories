608e25afc46dc76a688e30020d44912d17174086
Joachim Spalink <J.spalink@gmx.de>
2017-11-08 15:37:57 +0100

    compare works, but not updating cutted trajectory

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -10,79 +10,114 @@ import { QTree, Trajectory, TrajectoryViewType } from '../data-structures';
   styleUrls: ['./compare.component.css']
 })
 export class CompareComponent implements OnInit {
+  visibleData = [];
   someRange = 3;
-  private chartData: Array<any>;
+  private chartData1: Array<any> = [];
+  private chartData2: Array<any> = [];
   cutPoints: [number, number];
   nrLines: number;
   minQuadsize = 20;
   simScores;
-  stimuli2 = [];
-  stimuli1 = [];
+  stimuli2_hist: Array<TrajectoryViewType> = [];
+  stimuli1_hist: Array<TrajectoryViewType> = [];
+  private mm = new MultiMatch();
   viewAsQuadtree: boolean;
-  selected_participants = Array<{ name: string, id: number }>();
-  selected_stimuli = Array<{ name: string, id: number }>();
-  resolutionName = '/assets/resolution.txt';
-  filenameall = 'assets/all_fixation_data_cleaned_up.csv';
-  filename = 'assets/small_fix_data_cleaned.csv';
+  selected_stimuli1: Trajectory;
+  selected_stimuli2: Trajectory;
+
   filteredFixData: Array<TrajectoryViewType> = [];
-  resolutions: Array<{ city: string, height: number, width: number }> = [];
+
   qTree: QTree;
   simMatrix: number[][];
-  constructor(public ngProgress: NgProgress, private selectionService: SelectionService, private dataService: DataService) {
-    const data = [['Index 0', 0.1], ['Index 1', 0.2], ['Index 2', 0.3], ['Index 3', 0.4],
-    ['Index 4', 0.5], ['Index 5', 0.6], ['Index 6', 0.7], ['Index 7', 0.8], ['Index 8', 0.9], ['Index 9', 1]];
-    this.chartData = data;
-  }
+  constructor(public ngProgress: NgProgress, private selectionService: SelectionService, private dataService: DataService) { }
 
   ngOnInit() {
     if (!this.dataService.dataLoaded) {
       console.log('load data');
       this.dataService.loadData(this.dataService.filename, this.dataService.resolutionName);
     }
-    this.selected_stimuli = this.selectionService.getSelectedSimuli();
-    this.selected_participants = this.selectionService.getSelectedParticipants();
-
     this.filteredFixData = this.dataService.filterData({ asQuad: this.viewAsQuadtree, level: this.someRange });
-    this.stimuli1 = this.filteredFixData.map((d, i) => ({ id: i, name: d.participant + ' ' + d.stimulus }));
-    this.stimuli2 = this.filteredFixData.map((d, i) => ({ id: i, name: d.participant + ' ' + d.stimulus }));
+    this.visibleData = this.filteredFixData;
   }
-  generateData() {
-    this.chartData = [];
-    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
-      this.chartData.push([`Index ${i}`, Math.floor(Math.random() * 100)]);
+
+  onChange1(v) {
+    console.log(v);
+    this.selected_stimuli1 = v;
+    this.visibleData = [];
+    if (this.selected_stimuli2) {
+      this.visibleData = [this.selected_stimuli1, this.selected_stimuli2];
+      this.clearStimHist();
+      this.stimuli1_hist.push(this.selected_stimuli1);
+      this.stimuli2_hist.push(this.selected_stimuli2);
+      this.calcScores(this.selected_stimuli1, this.selected_stimuli2);
     }
   }
 
-  findCorrespondingPair(clusterIndex: number) {
-    const row = clusterIndex % this.simScores.length;
-    const col = clusterIndex / this.simScores[0].length;
-    return this.simScores[row][col + row];
-  }
-  computeSimScores() {
-    const simScores = [];
-    this.ngProgress.start();
-    const mm = new MultiMatch();
-    console.log('start mm');
-    for (let i = 0; i < this.filteredFixData.length; i++) {
-      simScores[i] = [];
-      for (let k = i + 1; k < this.filteredFixData.length; k++) {
-        simScores[i][k] = {
-          p1: this.filteredFixData[i].participant,
-          s1: this.filteredFixData[i].stimulus,
-          p2: this.filteredFixData[k].participant,
-          s2: this.filteredFixData[k].stimulus,
-          scores: mm.compare(this.filteredFixData[i].points, this.filteredFixData[k].points, calcdiag(1651, 1200)),
-          cluster: 'none'
-        };
-        this.ngProgress.set((i * k) / ((this.filteredFixData.length * this.filteredFixData.length) / 2));
-      }
+  onChange2(v) {
+    console.log(v);
+    this.selected_stimuli2 = v;
+    this.visibleData = [];
+    if (this.selected_stimuli1) {
 
+      this.visibleData = [this.selected_stimuli1, this.selected_stimuli2];
+      this.clearStimHist();
+      this.stimuli1_hist.push(this.selected_stimuli1);
+      this.stimuli2_hist.push(this.selected_stimuli2);
+      this.calcScores(this.selected_stimuli1, this.selected_stimuli2);
     }
-    this.ngProgress.done();
-    return simScores;
   }
-  cutTraj() {
+  clearStimHist() {
+    this.stimuli1_hist = [];
+    this.stimuli2_hist = [];
+  }
+  calcScores(t1: TrajectoryViewType, t2: TrajectoryViewType) {
+    this.chartData1 = [];
+    this.chartData2 = [];
+    for (let i = 0; i < t1.points.length; i++) {
+      this.chartData1.push([`${i}`, Object.values(this.mm.compare(t1.points.filter((v, index) => i !== index),
+        t2.points, calcdiag(1651, 1200))).reduce((sum, current) => sum + current) / 5]);
+    }
+    for (let i = 0; i < t2.points.length; i++) {
+      this.chartData2.push([`${i}`, Object.values(this.mm.compare(t2.points.filter((v, index) => i !== index),
+        t1.points, calcdiag(1651, 1200))).reduce((sum, current) => sum + current) / 5]);
+    }
+  }
 
+  cutTraj(traj: string) {
+    this.visibleData = [];
+    console.log(traj);
+    console.log(this.cutPoints);
+    const p = { ...this.selected_stimuli1.points };
+    let removedPoints;
+    const numberOfPoints = this.cutPoints[1] - this.cutPoints[0];
+    console.log(numberOfPoints);
+    if (traj === 'first') {
+      console.log(p);
+      removedPoints = this.selected_stimuli1.points.splice(this.cutPoints[0], numberOfPoints === 0 ? 1 : numberOfPoints + 1);
+      const modifiedstimulus = {
+        color: this.selected_stimuli1.color,
+        participant: this.selected_stimuli1.participant,
+        stimulus: this.selected_stimuli1.stimulus,
+        points: this.selected_stimuli1.points
+      };
+      console.log(this.selected_stimuli1.points);
+      this.stimuli1_hist.push(removedPoints);
+
+      this.visibleData = [modifiedstimulus, this.selected_stimuli2];
+      // console.log(modifiedstimulus, this.selected_stimuli2.points);
+    } else {
+      removedPoints = this.selected_stimuli2.points.splice(this.cutPoints[0], numberOfPoints === 0 ? 1 : numberOfPoints + 1);
+      const modifiedstimulus = {
+        color: this.selected_stimuli2.color,
+        participant: this.selected_stimuli2.participant,
+        stimulus: this.selected_stimuli2.stimulus,
+        points: this.selected_stimuli2.points
+      };
+      this.stimuli2_hist.push(removedPoints);
+      this.visibleData = [modifiedstimulus, this.selected_stimuli1];
+      // console.log(modifiedstimulus, this.selected_stimuli1.points);
+    }
+    this.calcScores(this.visibleData[0], this.visibleData[1]);
   }
   values(v) {
     this.cutPoints = v;
