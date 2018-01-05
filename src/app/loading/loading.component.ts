530d709db952436d9e86d26d121573fb2f83f1f1
Joachim Spalink <J.spalink@gmx.de>
2018-01-05 14:32:54 +0100

    loading for local files

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -22,21 +22,25 @@ export class LoadingComponent implements OnInit {
     this.loading = true;
     this.dataService.loadData(this.trajectoryName, this.resolutionName);
   }
-  public onAction(event: any) {
+  public onActionTraj(event: any) {
     console.log(event);
     this.actionLog += '\n currentFiles: ' + this.getFileNames(event.currentFiles);
+    console.log(this.getTempPaths(event.currentFiles));
+    this.trajectoryName = this.getTempPaths(event.currentFiles)[0];
     console.log(this.actionLog);
   }
-  public onAdded(event: any) {
-    this.actionLog += '\n FileInput: ' + event.id;
-    this.actionLog += '\n Action: File added';
-  }
-  public onRemoved(event: any) {
-    this.actionLog += '\n FileInput: ' + event.id;
-    this.actionLog += '\n Action: File removed';
+  public onActionRes(event: any) {
+    console.log(event);
+    this.actionLog += '\n currentFiles: ' + this.getFileNames(event.currentFiles);
+    console.log(this.getTempPaths(event.currentFiles));
+    this.resolutionName = this.getTempPaths(event.currentFiles)[0];
+    console.log(this.actionLog);
   }
   private getFileNames(files: File[]): string {
     const names = files.map(file => file.name);
     return names ? names.join(', ') : 'No files currently added.';
+  }
+  private getTempPaths(files: File[]): string[] {
+    return files.map(file => URL.createObjectURL(file));
   }
 }
